12318144287402694a64169a8112aa3f185c46c2
FalkZ <24669719+FalkZ@users.noreply.github.com>
2019-01-07 12:50:07 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+import passepartout from 'passepartout';
+
+import lighthouse from './lighthouse.json';
+
+import model from './model';
+
+const view = ({ model: { lighthouse, watts, seconds } }) => {
+  console.log(lighthouse.audits.interactive);
+
+  return [
+    [
+      { _: 'h1', class: 'sec' },
+      Math.round(lighthouse.audits.interactive.rawValue / 100) / 10,
+      [{ _: 'p' }, lighthouse.audits.interactive.displayValue],
+      [{ _: 'p' }, lighthouse.audits.interactive.rawValue * watts, 'mWh']
+    ]
+  ];
+};
+passepartout({ model, view });
---
+++ model.js
@@ -0,0 +1,3 @@
+import lighthouse from './lighthouse.json';
+
+export default { lighthouse, watts: 8.6, seconds: 13 };
